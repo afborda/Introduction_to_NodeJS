ddc199b821737eb727b7637933e6efd2aa667bd0
Estudos <estudos@Abners-MacBook-Pro.local>
2023-03-15 23:18:36 -0300

    POST, PUT, DELETE, GET, Filter > OK

---
+++ src/routes/routes.js
@@ -0,0 +1,63 @@
+import { randomUUID } from 'node:crypto'
+import { Database } from '../database/database.js'
+import { buildRouteParh } from '../utils/build-route-path.js.js'
+
+const database = new Database()
+
+export const routes = [
+  {
+    method: 'GET',
+    path: buildRouteParh('/users'),
+    handler: (req, res) => {
+      const { search } = req.query
+
+      const users = database.select(
+        'users',
+        search
+          ? {
+            name: search,
+            email: search
+          }
+          : null
+      )
+      return res.end(JSON.stringify(users))
+    }
+  },
+  {
+    method: 'POST',
+    path: buildRouteParh('/users'),
+    handler: (req, res) => {
+      const { name, email } = req.body
+      const user = {
+        id: randomUUID(),
+        name,
+        email
+      }
+      database.insert('users', user)
+      return res.writeHead(201).end()
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRouteParh(`/users/:id`),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { name, email } = req.body
+
+      database.update('users', id, {
+        name,
+        email
+      })
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRouteParh(`/users/:id`),
+    handler: (req, res) => {
+      const { id } = req.params
+      database.delete('users', id)
+      return res.writeHead(204).end()
+    }
+  }
+]
